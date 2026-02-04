76a1fe8f80766adf80195db3a5e916ac49fbf895
HECTOR MARQUEZ <marquezgot@gmail.com>
2026-02-04 08:38:06 -0500

    Update 2️⃣ 02_basic_dataframe.py

--- 01_spark_basics/2️⃣ 02_basic_dataframe.py
+++ 01_spark_basics/2️⃣ 02_basic_dataframe.py
@@ -1,1 +1,31 @@
 
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("BasicDataFrame") \
+    .getOrCreate()
+
+# Datos simulados (como si vinieran de una BD o archivo)
+data = [
+    (1, "Ana", 1200),
+    (2, "Luis", 800),
+    (3, "Carlos", 1500),
+    (4, "Ana", 1200)
+]
+
+columns = ["id", "nombre", "monto"]
+
+# Crear DataFrame
+df = spark.createDataFrame(data, columns)
+
+# Mostrar datos
+df.show()
+
+# Ver esquema
+df.printSchema()
+
+# Seleccionar columnas
+df.select("nombre", "monto").show()
+
+# Filtrar datos
+df.filter(df.monto > 1000).show()
